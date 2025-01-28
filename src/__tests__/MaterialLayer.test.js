791101342e93a33e02fdec16a29d6a239de1a0ca
BenAfter <notz968@gmail.com>
2025-01-28 14:31:17 +0700

    MaterialLayer.test.js

--- src/__tests__/MaterialLayer.test.js
+++ src/__tests__/MaterialLayer.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from './utils/testUtils';
 import MaterialLayer from '../components/MaterialLayer';
 
 describe('MaterialLayer', () => {
-  test('adds new layer', () => {
+  test('adds layer with default values', () => {
     const handleUpdate = jest.fn();
-    render(<MaterialLayer onLayerUpdate={handleUpdate} />);
+    const { getByText } = render(<MaterialLayer onLayerUpdate={handleUpdate} />);
     
-    fireEvent.click(screen.getByText('Add Layer'));
-    expect(handleUpdate).toHaveBeenCalled();
+    fireEvent.click(getByText('Add Layer'));
+    expect(handleUpdate).toHaveBeenCalledWith([{
+      material: 'wood',
+      thickness: 1,
+      cost: 0
+    }]);
   });
 });
